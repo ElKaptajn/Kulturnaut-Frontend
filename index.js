87a2ad97b11b009a14a201593e56e02400759d04
xenius <xeni0103@stud.kea.dk>
2022-11-06 13:53:22 +0100

    Login, sign up og add abnd virker

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function signup(event) {
     console.log(payload)
     fetch("http://localhost:8080/signup",
         {
-            mode: 'no-cors',
             method: "POST",
             body: payload,
             headers:{'content-type': 'application/json'}
@@ -29,11 +28,60 @@ function login(event) {
     payload = JSON.stringify(payload)
     fetch("http://localhost:8080/login",
         {
-            mode: 'no-cors',
             method: "POST",
             body: payload,
             headers:{'content-type': 'application/json'}
         })
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (res) {
+            localStorage.setItem('user', JSON.stringify(res));
+        })
 }
 
+function logout(event) {
+    event.preventDefault()
+    const div = document.getElementById('container');
+    localStorage.removeItem('user')
+}
 
+function createBand(event){
+    event.preventDefault()
+    const bandNameField = document.getElementById("bandNameField").value;
+
+    let payload = {name: bandNameField};
+
+    payload = JSON.stringify(payload);
+
+    const localstorage_user = JSON.parse(localStorage.getItem('user'));
+    const inMemoryToken = localstorage_user.token;
+
+    fetch("http://localhost:8080/createBand",
+        {
+            method: "POST",
+            body: payload,
+            headers:{'content-type': 'application/json',
+                        'Authorization': 'Bearer ' + inMemoryToken }
+        });
+}
+
+function createEvent(event){
+    event.preventDefault()
+    const eventField = document.getElementById("eventField").value;
+
+    let payload = {name: eventField};
+
+    payload = JSON.stringify(payload);
+
+    const localstorage_user = JSON.parse(localStorage.getItem('user'));
+    const inMemoryToken = localstorage_user.token;
+
+    fetch("http://localhost:8080/createEvent",
+        {
+            method: "POST",
+            body: payload,
+            headers:{'content-type': 'application/json',
+                        'Authorization': 'Bearer ' + inMemoryToken }
+        });
+}
